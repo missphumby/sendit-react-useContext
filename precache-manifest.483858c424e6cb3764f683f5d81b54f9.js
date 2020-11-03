c20dc0e82bfe1e07230bff5a2807e259e0725320
Funby <jamiuaminat@yahoo.com>
2020-11-03 17:54:12 +0100

    Updates

---
+++ precache-manifest.483858c424e6cb3764f683f5d81b54f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc1a8ae6c1e03076be80e63c48a7a8a",
+    "url": "/sendit-react-useContext/index.html"
+  },
+  {
+    "revision": "95fe2aa1c3f56eed4d1d",
+    "url": "/sendit-react-useContext/static/css/2.6bde65ae.chunk.css"
+  },
+  {
+    "revision": "3b0eaea04033db735c97",
+    "url": "/sendit-react-useContext/static/css/main.e785ebb3.chunk.css"
+  },
+  {
+    "revision": "95fe2aa1c3f56eed4d1d",
+    "url": "/sendit-react-useContext/static/js/2.6ad33527.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sendit-react-useContext/static/js/2.6ad33527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0eaea04033db735c97",
+    "url": "/sendit-react-useContext/static/js/main.bc098c45.chunk.js"
+  },
+  {
+    "revision": "5fec27f105be083201f4",
+    "url": "/sendit-react-useContext/static/js/runtime-main.47a85716.js"
+  },
+  {
+    "revision": "51380477d291566f87a191c52be37f49",
+    "url": "/sendit-react-useContext/static/media/banner-1.51380477.jpg"
+  },
+  {
+    "revision": "429da4fd3270f223a084cb1c66337137",
+    "url": "/sendit-react-useContext/static/media/banner-2.429da4fd.jpg"
+  },
+  {
+    "revision": "4ce9057416af60a42fc5658347694f52",
+    "url": "/sendit-react-useContext/static/media/banner-3.4ce90574.jpg"
+  },
+  {
+    "revision": "b17716d485a4a37ef9c672b782b87f14",
+    "url": "/sendit-react-useContext/static/media/send-logo.b17716d4.png"
+  }
+]);
